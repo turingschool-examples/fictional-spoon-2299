3fa2cf5751c837af76db48266178b7baaacccc04
Chris Simmons <93938306+cjsim89@users.noreply.github.com>
2023-02-10 15:26:06 -0500

    Update recipe_spec.rb

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  describe 'validations' do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :complexity}
-    it {should validate_presence_of :genre}
-  end
+#   describe 'validations' do
+#     it {should validate_presence_of :name}
+#     it {should validate_presence_of :complexity}
+#     it {should validate_presence_of :genre}
+#   end
 
   describe 'relationships' do
     it {should have_many :recipe_ingredients}
