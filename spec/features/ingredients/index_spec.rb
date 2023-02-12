8a48b699bf0c0134a9568863f8061fe371c44ce8
Matt Enyeart <menyeart1@gmail.com>
2023-02-12 13:28:37 -0700

    Create ingredients index spec and initial test for attributes

---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'the ingredients index page' do
+  it 'lists all the ingredients and their attribues' do
+    meat = Ingredient.create!(name: 'meat', cost: 20)
+    vege = Ingredient.create!(name: 'vegetable', cost: 10)
+    pasta = Ingredient.create!(name: 'pasta', cost: 5)
+
+    visit '/ingredients'
+    save_and_open_page
+    expect(page).to have_content("meat : 20")
+    expect(page).to have_content("vegetable : 10")
+    expect(page).to have_content("pasta : 5")
+  end
+end
