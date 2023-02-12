f3cd46b0f8bfe237f8cd6e62da6125848867187c
Tori Enyart <torienyart@gmail.com>
2023-02-12 16:03:09 -0700

    feat: Create page to show a Recipe and its ingredients

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe 'As a visitor' do
   describe "When I visit '/recipes/:id'" do
     it "I see the recipe's name, complexity and genre," do
-      beef = Ingredient.create!(name: "ground beef", cost: 5)
-      sauce = Ingredient.create!(name: "tomato sauce", cost: 4)
-      noods = Ingredient.create!(name: "spaghetti noodles", cost: 3)
       spaghetti = Recipe.create!(name: "Spaghetti Bolognese", complexity: 1, genre: "Italian")
+      beef = spaghetti.ingredients.create!(name: "ground beef", cost: 5)
+      sauce = spaghetti.ingredients.create!(name: "tomato sauce", cost: 4)
+      noods = spaghetti.ingredients.create!(name: "spaghetti noodles", cost: 3)
 
       visit "/recipes/#{spaghetti.id}"
 
@@ -16,10 +16,10 @@ describe 'As a visitor' do
     end
 
     it "I see a list of the names of the ingredients for the recipe." do
-      beef = Ingredient.create!(name: "ground beef", cost: 5)
-      sauce = Ingredient.create!(name: "tomato sauce", cost: 4)
-      noods = Ingredient.create!(name: "spaghetti noodles", cost: 3)
       spaghetti = Recipe.create!(name: "Spaghetti Bolognese", complexity: 1, genre: "Italian")
+      beef = spaghetti.ingredients.create!(name: "ground beef", cost: 5)
+      sauce = spaghetti.ingredients.create!(name: "tomato sauce", cost: 4)
+      noods = spaghetti.ingredients.create!(name: "spaghetti noodles", cost: 3)
 
       visit "/recipes/#{spaghetti.id}"
 
@@ -32,3 +32,4 @@ describe 'As a visitor' do
     end
   
   end
+end
