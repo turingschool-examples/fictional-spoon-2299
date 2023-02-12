f9fd708637273df54749cf79be846eac8034f696
Adam Bailey <ab67319@gmail.com>
2023-02-12 10:24:38 -0700

    feat: create class method ::total_cost

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,8 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
+
+  def self.total_cost
+    Ingredient.sum(:cost)
+  end
 end
