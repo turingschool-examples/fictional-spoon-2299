1ae6da9ec1027d6399ab3a66439bbba168056567
KaraJH <karas.jones@yahoo.com>
2023-02-12 13:21:59 -0700

    feat: Order ingredients alphabetically with passing test

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,9 @@
 class Ingredient < ApplicationRecord
-    has_many :recipe_ingredients
-    has_many :recipes, through: :recipe_ingredients
+  has_many :recipe_ingredients
+  has_many :recipes, through: :recipe_ingredients
+
+  def self.order_by_alpha
+    Ingredient.order(:name)
+  end
+  
 end
