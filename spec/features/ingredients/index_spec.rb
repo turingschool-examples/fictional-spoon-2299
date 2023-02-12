112cf423df668158fbaf79c8b293b7a7a2bb43b6
Andra Helton <andrahelton@gmail.com>
2023-02-12 13:38:59 -0700

    Add debugging for Ingredient index spec

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
-
-RSpec.describe Ingredient, type: feature do
+RSpec.describe 'Ingredient Show Page' do
 
   let!(:ingredient_1) { Ingredient.create!(name: "Croutons", cost: 4) }
   let!(:ingredient_2) { Ingredient.create!(name: "Beef", cost: 200) }
 
-  it 'shows attributes of all ingredients' do\
+  it 'shows attributes of all ingredients' do
     visit "/ingredients"
 
     expect(page).to have_content(ingredient_1.name)
