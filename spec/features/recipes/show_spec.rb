0307e568b8ebbc74248b4c62db09e05fc42f37f9
Weston Sandfort <sandfortw@gmail.com>
2023-02-12 12:42:31 -0700

    add test for user story 3

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -28,4 +28,23 @@ describe 'recipe show page' do
     expect(page).to have_content(tomato.name)
     expect(page).to have_content(bean.name)
   end
+
+#   User Story 3
+
+# As a visitor,
+# When I visit '/recipes/:id'
+# I see the total cost of all of the ingredients in the recipe.
+# (e.g. "Total Cost: 22")
+
+  it 'displays the total cost' do
+    recipe = Recipe.create!(name: 'Chili', complexity: 1, genre: 'southwestern')
+    pepper = Ingredient.create!(name: "pepper", cost: 10)
+    tomato = Ingredient.create!(name: "whole-peeled tomatoes", cost: 1)
+    bean = Ingredient.create!(name: "kidney beans", cost: 20)
+    ring1 = RecipeIngredient.create!(recipe_id: recipe.id, ingredient_id: pepper.id)
+    ring2 = RecipeIngredient.create!(recipe_id: recipe.id, ingredient_id: tomato.id)
+    ring3 = RecipeIngredient.create!(recipe_id: recipe.id, ingredient_id: bean.id)
+    visit "/recipes/#{recipe.id}"
+    expect(page).to have_content("Total Cost: 31")
+  end
 end
