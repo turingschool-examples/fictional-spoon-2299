ce3b1dbcdcec682e215a79bb4ef16a90e0899143
Weston Sandfort <sandfortw@gmail.com>
2023-02-12 13:02:49 -0700

    add model test for Ingredient.alphabetically

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe Ingredient, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:recipes).through(:recipe_ingredients)}
   end
+
+  describe '.alphabetically' do
+    it 'sorts self alphabetically' do
+      pepper = Ingredient.create!(name: "pepper", cost: 10)
+      milk = Ingredient.create!(name: "milk", cost: 1)
+      sausage = Ingredient.create!(name: "sausage", cost: 20)
+      a = Ingredient.create!(name: "a", cost: 20)
+      z = Ingredient.create!(name: "z", cost: 20)
+      expect(Ingredient.alphabetically).to eq([a, milk, pepper, sausage, z])
+    end
+  end
 end
