ed8d31367c644c8c0bd3c1dce6c20c112e59b86d
Weston Sandfort <sandfortw@gmail.com>
2023-02-12 13:53:09 -0700

    add seed data and formatted ingredients index page

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,34 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_704_172_317) do
+ActiveRecord::Schema.define(version: 2022_07_04_172317) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'ingredients', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'cost'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.integer "cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'recipe_ingredients', force: :cascade do |t|
-    t.bigint 'recipe_id'
-    t.bigint 'ingredient_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['ingredient_id'], name: 'index_recipe_ingredients_on_ingredient_id'
-    t.index ['recipe_id'], name: 'index_recipe_ingredients_on_recipe_id'
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.bigint "recipe_id"
+    t.bigint "ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_recipe_ingredients_on_recipe_id"
   end
 
-  create_table 'recipes', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'complexity'
-    t.string 'genre'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.integer "complexity"
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'recipe_ingredients', 'ingredients'
-  add_foreign_key 'recipe_ingredients', 'recipes'
+  add_foreign_key "recipe_ingredients", "ingredients"
+  add_foreign_key "recipe_ingredients", "recipes"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+    recipe = Recipe.create!(name: 'Chili', complexity: 1, genre: 'southwestern')
+    pepper = Ingredient.create!(name: 'pepper', cost: 10)
+    tomato = Ingredient.create!(name: 'whole-peeled tomatoes', cost: 1)
+    bean = Ingredient.create!(name: 'kidney beans', cost: 20)
+    RecipeIngredient.create!(recipe_id: recipe.id, ingredient_id: pepper.id)
+    RecipeIngredient.create!(recipe_id: recipe.id, ingredient_id: tomato.id)
+    RecipeIngredient.create!(recipe_id: recipe.id, ingredient_id: bean.id)
