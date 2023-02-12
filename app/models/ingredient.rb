7cc58378091092e006ffcb2ba689b4cae406e64e
Weston Sandfort <sandfortw@gmail.com>
2023-02-12 13:44:48 -0700

    add validations back in for ingredient in test and in model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,6 +3,8 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
+  validates :name, presence: true
+  validates :cost, presence: true
 
   def self.alphabetically
     order(:name)
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -3,10 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  #   describe 'validations' do
-  #     it {should validate_presence_of :name}
-  #     it {should validate_presence_of :cost}
-  #   end
+    describe 'validations' do
+      it {should validate_presence_of :name}
+      it {should validate_presence_of :cost}
+    end
 
   describe 'relationships' do
     it { should have_many :recipe_ingredients }
