ba7b770248a17a14736f1580a622c5fa8a8b87bf
KaraJH <karas.jones@yahoo.com>
2023-02-12 13:14:07 -0700

    test: Ingredients total cost model test

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
+  let!(:good_pasta) {Recipe.create!(name: "Good Pasta", complexity: 2, genre: "Italian")}
+  let!(:rosemary) {good_pasta.ingredients.create!(name: "Rosemary", cost: 1)}
+  let!(:tomato) {good_pasta.ingredients.create!(name: "Tomato", cost: 4)}
+  let!(:pasta) {good_pasta.ingredients.create!(name: "Pasta", cost: 2)}
 #   describe 'validations' do
 #     it {should validate_presence_of :name}
 #     it {should validate_presence_of :complexity}
@@ -10,5 +14,11 @@ RSpec.describe Recipe, type: :model do
   describe 'relationships' do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
+  end
+
+  describe 'ingredients_total_cost' do 
+    it 'calculates the total cost of ingredients for the recipe' do 
+      expect(good_pasta.ingredients_total_cost).to eq(7)
+    end
   end
 end
