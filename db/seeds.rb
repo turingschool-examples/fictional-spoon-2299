e192487e1e98c4e0c863d4ecc41710c206ff1e6c
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-02-12 10:28:37 -0800

    add: seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@ratatouille = Recipe.create!(name: "Ratatouille", complexity: 3, genre: "Italian")
+@sandwich = Recipe.create!(name: "Sandwich", complexity: 1, genre: "Broke Food")
+@mac_and_cheese = Recipe.create!(name: "Mac-and-cheese", complexity: 2, genre: "Broke Food")
+
+@tomato = @ratatouille.ingredients.create!(name: "tomato", cost: 2)
+@salt = @ratatouille.ingredients.create!(name: "salt", cost: 3)
+@turkey = @ratatouille.ingredients.create!(name: "turkey", cost: 15)
+@bread = @sandwich.ingredients.create!(name: "bread", cost: 5)
+@mayo = @sandwich.ingredients.create!(name: "mayonnaise", cost: 7)
+@bologna = @sandwich.ingredients.create!(name: "bologna", cost: 11)
+@milk = @mac_and_cheese.ingredients.create!(name: "milk", cost: 6)
+@cheese = @mac_and_cheese.ingredients.create!(name: "cheese powder", cost: 5)
+@pasta = @mac_and_cheese.ingredients.create!(name: "pasta", cost: 7)
