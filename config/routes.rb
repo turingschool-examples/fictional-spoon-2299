6dfd5f0b58edd0aba501d0d8e8a513d34b9aa548
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-02-12 16:21:08 -0700

    Add feature to add ingredient to recipe by id and update page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 	get '/ingredients', to: 'ingredients#index'
 
 	get '/recipes/:id', to: 'recipes#show'
+	
+	post '/recipeingredients', to: 'recipe_ingredients#create'
 end
--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Recipes Show Page', type: :feature do
 		let!(:flour) { Ingredient.create!(name: 'Flour', cost: 2) }
 		let!(:eggs) { Ingredient.create!(name: 'Eggs', cost: 6) }
 		let!(:butter) { Ingredient.create!(name: 'Butter', cost: 3) }
+		let!(:sugar) { Ingredient.create!(name: 'Sugar', cost: 2)}
 		
 		let!(:biscuits) { Recipe.create!(name: 'Biscuits', complexity: 1, genre: 'Bread') }
 \
@@ -33,9 +34,20 @@ RSpec.describe 'Recipes Show Page', type: :feature do
 		it 'I see a form to add an ingredient to the recipe' do
 			visit "/recipes/#{biscuits.id}"
 			
-			expect(page).to have_field(:name)
-			expect(page).to have_field(:cost)
+			expect(page).to have_field(:ingredient_id)
 			expect(page).to have_button('Add Ingredient')
+		end
+
+		it 'When I fill in the ingredient ID field and click submit, I am redirected to show page and see new ingredient' do
+			visit "/recipes/#{biscuits.id}"
+
+			expect(page).to_not have_content(sugar.name)
+			
+			fill_in 'ingredient_id', with: "#{sugar.id}"
+			click_button 'Add Ingredient'
+
+			expect(current_path).to eq("/recipes/#{biscuits.id}")
+			expect(page).to have_content(sugar.name)
 		end
 	end
 end
