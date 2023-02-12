f6652e7f31b2491a52484b17af12c54b9193dcb3
Weston Sandfort <sandfortw@gmail.com>
2023-02-12 12:50:09 -0700

    add model test for recipe#cost

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe Recipe, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
   end
+
+  describe '#cost' do
+    it 'totals the cost of the ingredients for a recipe' do
+      recipe = Recipe.create!(name: 'Chili', complexity: 1, genre: 'southwestern')
+      pepper = Ingredient.create!(name: "pepper", cost: 10)
+      tomato = Ingredient.create!(name: "whole-peeled tomatoes", cost: 1)
+      bean = Ingredient.create!(name: "kidney beans", cost: 20)
+      ring1 = RecipeIngredient.create!(recipe_id: recipe.id, ingredient_id: pepper.id)
+      ring2 = RecipeIngredient.create!(recipe_id: recipe.id, ingredient_id: tomato.id)
+      ring3 = RecipeIngredient.create!(recipe_id: recipe.id, ingredient_id: bean.id)
+      expect(recipe.cost).to eq(31)
+    end
+  end
 end
