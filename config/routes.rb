51fc5a1a40c176c186e168098e8201c179548ca0
Matt Enyeart <menyeart1@gmail.com>
2023-02-12 15:43:24 -0700

    Add route for new recipe ingredient creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   get '/ingredients', to: 'ingredients#index'
   get '/recipes/:id', to: 'recipes#show'
+  # post '/recipes/:id/ingredients', to: 'recipe_ingredients#create'
 end
 
 
