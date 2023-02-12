8914440cf59bc49d0b827359db6294b79da31ec8
Hady Matar <hadyematar@gmail.com>
2023-02-12 07:04:46 -0700

    All 6 tests passing

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,7 @@
 class Ingredient < ApplicationRecord
+  has_many(:recipe_ingredients)
+  has_many(:recipes, through: :recipe_ingredients)
+
+  validates :name, presence: true
+  validates :cost, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ApplicationRecord
+
+  has_many(:recipe_ingredients)
+  has_many(:ingredients, through: :recipe_ingredients)
+
 end
