a9460f501a9a7f804d8a7aa828551657b5e1c513
Andra Helton <andrahelton@gmail.com>
2023-02-12 13:02:35 -0700

    Add passing activerecord association tests

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,5 @@
 class Recipe < ApplicationRecord
-
   has_many :recipe_ingredients
-  # has_many :ingredients, through: :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
   validates :name, :complexity, :genre, presence: true
 end
