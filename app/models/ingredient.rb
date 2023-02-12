ae712caa1fb5bb5ce4410b0418d69651f317cd7f
Matt Enyeart <menyeart1@gmail.com>
2023-02-12 15:06:33 -0700

    Create sort by name alpha method

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,8 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
+
+  def self.sort_by_name_alpha
+    order(name: :asc)
+  end  
 end
