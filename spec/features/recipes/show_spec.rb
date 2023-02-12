56fa7dbf731cba1b35e8709b996d17b749706643
Matt Enyeart <menyeart1@gmail.com>
2023-02-12 14:22:23 -0700

    Add a test for displaying a recipes total cost in recipe show view

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -13,10 +13,29 @@ RSpec.describe 'the recipes show page' do
     recing4 = RecipeIngredient.create!(ingredient_id: vege.id, recipe_id: kebabs.id)
 
     visit "/recipes/#{spagetti.id}"
-    save_and_open_page
-    expect(page).to have_content('meat')
-    expect(page).to have_content('meat')
-    expect(page).to have_content('meat')
 
+    expect(page).to have_content('Recipe Name: spagetti')
+    expect(page).to have_content('Complexity: 1')
+    expect(page).to have_content('Genre: Italian')
+    expect(page).to have_content('meat')
+    expect(page).to have_content('pasta')
+
+  end
+
+  it "lists a recipe's total ingredient cost" do
+    meat = Ingredient.create!(name: 'meat', cost: 20)
+    vege = Ingredient.create!(name: 'vegetable', cost: 10)
+    pasta = Ingredient.create!(name: 'pasta', cost: 5)
+    spagetti = Recipe.create!(name: 'spagetti', complexity: 1, genre: 'Italian')
+    kebabs = Recipe.create!(name: 'kebabs', complexity: 1, genre: 'Mediterranean')
+    recing1 = RecipeIngredient.create!(ingredient_id: meat.id, recipe_id: spagetti.id)
+    recing2 = RecipeIngredient.create!(ingredient_id: pasta.id, recipe_id: spagetti.id)
+    recing3 = RecipeIngredient.create!(ingredient_id: meat.id, recipe_id: kebabs.id)
+    recing4 = RecipeIngredient.create!(ingredient_id: vege.id, recipe_id: kebabs.id)
+
+    visit "/recipes/#{spagetti.id}"
+    save_and_open_page
+
+    expect(page).to have_content("Total ingredient cost: 25")
   end
 end
