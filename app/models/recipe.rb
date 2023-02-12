31555872be48b0663bc78e9622539668f505a420
KaraJH <karas.jones@yahoo.com>
2023-02-12 13:14:35 -0700

    feat: model total cost with passing test

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients 
   has_many :ingredients, through: :recipe_ingredients
+
+  def ingredients_total_cost 
+    ingredients.sum(:cost)
+  end
 end
