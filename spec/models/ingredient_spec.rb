973c8a31c4c47ad5e615253d5e9c4cb1e10337b8
Matt Enyeart <menyeart1@gmail.com>
2023-02-12 15:07:28 -0700

    Add test for sort by name alpha method

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe Ingredient, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:recipes).through(:recipe_ingredients)}
   end
+
+  describe 'methods' do
+    it 'can list ingredients alphabetically' do
+      meat = Ingredient.create!(name: 'meat', cost: 20)
+      vege = Ingredient.create!(name: 'vegetable', cost: 10)
+      pasta = Ingredient.create!(name: 'pasta', cost: 5)
+      cheese = Ingredient.create!(name: 'cheese', cost: 15)
+  
+      expect(Ingredient.sort_by_name_alpha.to_a).to eq([cheese, meat, pasta, vege])
+    end
+  end  
 end
