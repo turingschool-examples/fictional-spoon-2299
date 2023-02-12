c59f718e419a43550b749feb8334d4f5d54474fc
Adam Bailey <ab67319@gmail.com>
2023-02-12 10:31:44 -0700

    refactor: update show page test to include total cost of recipe

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "Recipes Show Page" do
   describe "As a visitor" do
     describe "When I visit '/recipes/:id'" do
-      it 'can see the recipes attributes and a list of the names of each ingredient for the recipe' do
+      it 'can see the recipes attributes,a list of the names of each ingredient for the recipe and their total cost' do
         recipe = Recipe.create!(name: "Taco Meat", complexity: 1, genre: "Mexican")
         recipe.ingredients.create!(name: "Ground Beef", cost: 4)
         recipe.ingredients.create!(name: "Salt", cost: 1)
@@ -17,6 +17,7 @@ RSpec.describe "Recipes Show Page" do
         expect(page).to have_content("Complexity: 1")
         expect(page).to have_content("Genre: Mexican")
         expect(page).to have_content("Ingredients: Ground Beef, Salt, Taco Seasoning")
+        expect(page).to have_content("Total Cost: 7")
         expect(page).to_not have_content("Cereal")
       end
     end
