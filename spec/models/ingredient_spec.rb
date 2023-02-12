3ad605e7b8c784e891b3fb0ba3b28742c79f50e8
KaraJH <karas.jones@yahoo.com>
2023-02-12 13:21:31 -0700

    test: Order by alpha method

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
+  let!(:rosemary) {Ingredient.create!(name: "Rosemary", cost: 1)}
+  let!(:tomato) {Ingredient.create!(name: "Tomato", cost: 4)}
+  let!(:pasta) {Ingredient.create!(name: "Pasta", cost: 2)}
 #   describe 'validations' do
 #     it {should validate_presence_of :name}
 #     it {should validate_presence_of :cost}
@@ -9,5 +12,11 @@ RSpec.describe Ingredient, type: :model do
   describe 'relationships' do
     it {should have_many :recipe_ingredients}
     it {should have_many(:recipes).through(:recipe_ingredients)}
+  end
+
+  describe '::order_by_alpha' do 
+    it 'sorts the ingredients alphabetically' do 
+      expect(Ingredient.order_by_alpha).to eq([pasta, rosemary, tomato])
+    end
   end
 end
