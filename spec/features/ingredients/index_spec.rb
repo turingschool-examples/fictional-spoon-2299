837df16f98b48fbfe3bdce68be694573c3fd9e56
Meredith Trevino <meredithtrevino@gmail.com>
2023-02-12 12:25:50 -0700

    half way through user story 1

---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Ingredient, type: :feature do
+  let!(:gound_beef) { Ingredient.create!(name: 'Ground Beef', cost: 2)}
+  let!(:salt) { Ingredient.create!(name: 'Salt', cost: 4)}
+
+  describe "US1: as a visitor to '/ingredients'" do
+    it 'I see a list of all the ingredients including their name and cost' do
+      visit '/ingredients'
+
+      expect(page).to have_content("Ground Beef")
+      expect(page).to have_content("Salt")
+      expect(page).to have_content(2)
+      expect(page).to have_content(4)
+
+    end
+  end
+end
