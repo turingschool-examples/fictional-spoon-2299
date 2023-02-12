b5ce1ad5535eb08602de89ee2426fe68a489519f
Weston Sandfort <sandfortw@gmail.com>
2023-02-12 12:56:46 -0700

    add orderly to gemfile and write test for ingredients index for extension 1

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -16,4 +16,20 @@ describe 'ingredients index' do
     expect(page).to have_content("#{milk.name}: #{milk.cost}")
     expect(page).to have_content("#{sausage.name}: #{sausage.cost}")
   end
+
+  # Extension 1
+  # Ingredients Sorted Alphabetically
+
+  # As a visitor,
+  # When I visit '/ingredients',
+  # Then I see that the list of ingredients is sorted alphabetically by name
+
+  it 'has ingredients sorted alphabetically' do
+    pepper = Ingredient.create!(name: "pepper", cost: 10)
+    milk = Ingredient.create!(name: "milk", cost: 1)
+    sausage = Ingredient.create!(name: "sausage", cost: 20)
+    visit '/ingredients'
+    expect(milk.name).to appear_before(pepper.name)
+    expect(pepper.name).to appear_before(sausage.name)
+  end
 end
