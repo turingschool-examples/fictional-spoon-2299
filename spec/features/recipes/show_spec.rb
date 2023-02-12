dbf3e28841bac0fa353b7e46c4df6fde7a6dd540
KaraJH <karas.jones@yahoo.com>
2023-02-12 14:04:35 -0700

    test: Add an existing ingredient

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe "Recipes Show Page" do
   let!(:tomato) {good_pasta.ingredients.create!(name: "Tomato", cost: 4)}
   let!(:pasta) {good_pasta.ingredients.create!(name: "Pasta", cost: 2)}
 
+  let!(:garlic) {Ingredient.create!(name: "Garlic", cost: 1)}
+
   describe 'When visiting recipe show page(/recipes/:id)' do 
     it 'has receipes name, complexity, genre and the ingredients' do 
       visit "/recipes/#{good_pasta.id}"
@@ -22,6 +24,17 @@ RSpec.describe "Recipes Show Page" do
       visit "/recipes/#{good_pasta.id}"
 
       expect(page).to have_content("Total Cost: 7")
+    end
+
+    it 'has a form to add an ingredient by an existing ingredient id' do
+      visit "/recipes/#{good_pasta.id}"
+
+      expect(page).to have_content("Add Ingredient")
+      fill_in("Ingredient ID", with: "#{garlic.id}")
+      click_button("Submit")
+
+      expect(current_path).to eq("/recipes/#{good_pasta.id}")
+      expect(page).to have_content(garlic.name)
     end
   end
 end
