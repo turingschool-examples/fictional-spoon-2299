4530ea72a79ce3a6c9fbe4cddebd1fa5892158ec
Adam Bailey <ab67319@gmail.com>
2023-02-12 10:43:33 -0700

    refactor: update test to check for ingredients ordered alphabetically by name

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -3,14 +3,16 @@ require "rails_helper"
 RSpec.describe "Ingredients Index Page" do
   describe "As a visitor" do
     describe "When I visit '/ingredients'" do
-      it 'can see a list of all the ingredients including their name and cost' do
-        ingredient_1 = Ingredient.create!(name: "Ground Beef", cost: 2)
-        ingredient_2 = Ingredient.create!(name: "Salt", cost: 4)
+      it 'can see a list of all the ingredients including their name and cost, sorted alphabetically by name' do
+        ingredient_1 = Ingredient.create!(name: "Salt", cost: 4)
+        ingredient_2 = Ingredient.create!(name: "Ground Beef", cost: 2)
 
         visit '/ingredients'
         
         expect(page).to have_content("Ground Beef: 2")
         expect(page).to have_content("Salt: 4")
+
+        expect(ingredient_2.name).to appear_before(ingredient_1.name)
       end
     end
   end
