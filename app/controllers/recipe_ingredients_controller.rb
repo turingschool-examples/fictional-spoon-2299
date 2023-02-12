e37191ab7cdaaa942c367157b6e27775c891ad37
Matt Enyeart <menyeart1@gmail.com>
2023-02-12 15:44:45 -0700

    Create recipe ingredients controller and create method

---
+++ app/controllers/recipe_ingredients_controller.rb
@@ -0,0 +1,13 @@
+class RecipeIngredientsController < ApplicationController
+  # def create
+  #   binding.pry
+  #   RecipeIngredient.create!(recipe_ingredient_params)
+  #   redirect_to "/recipes/:#{recipe.id}"
+  # end
+
+
+  # private
+  # def recipe_ingredient_params
+  #   params.permit(:id, :ingredient_id)
+  # end
+end
