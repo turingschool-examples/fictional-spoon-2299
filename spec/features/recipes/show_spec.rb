48c21482469cc50f0146a6ebd14752ca1244cad5
Weston Sandfort <sandfortw@gmail.com>
2023-02-12 13:12:25 -0700

    add test for extension 2

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -47,4 +47,28 @@ describe 'recipe show page' do
     visit "/recipes/#{recipe.id}"
     expect(page).to have_content('Total Cost: 31')
   end
+
+  # Extension 2
+  # Add an Ingredient to a Recipe
+
+  # As a visitor
+  # When I visit '/recipes/:id'
+  # Then I see a form to add an ingredient to this recipe.
+  # When I fill in a field with an existing ingredient's ID,
+  # And I click submit,
+  # Then I am redirrected to the recipe's show page,
+  # And I see the new ingredient listed for this recipe.
+
+  it 'has a form to add an ingredient to this recipe' do
+    recipe = Recipe.create!(name: 'Chili', complexity: 1, genre: 'southwestern')
+    pepper = Ingredient.create!(name: 'pepper', cost: 10)
+    visit "/recipes/#{recipe.id}"
+    expect(page).to_not have_content(pepper.name)
+    expect(page).to have_content('Enter an Ingredient ID to add to this recipe:')
+    expect(page).to have_form(:ingredientadd)
+    fill_in(:ingredientadd, with: pepper.id)
+    click_on 'Submit'
+    expect(current_path).to eq("/recipes/#{recipe.id}")
+    expect(page).to have_content(pepper.name)
+  end
 end
