b2d179fdc4a3811f26469a22a1c34c1d78465d95
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-02-12 15:42:07 -0700

    Add recipe show page - User story 2

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe 'Recipes Show Page', type: :feature do
 		let!(:eggs) { Ingredient.create!(name: 'Eggs', cost: 6) }
 		let!(:butter) { Ingredient.create!(name: 'Butter', cost: 3) }
 		
-		let!(:biscuits) { Recipe.create!(name: 'Biscuits', complexity: 'easy', genre: 'Bread') }
-		let!(:cookies) { Recipe.create!(name: 'Sugar Cookies', complexity: 'easy', genre: 'dessert') }
+		let!(:biscuits) { Recipe.create!(name: 'Biscuits', complexity: 1, genre: 'Bread') }
+		let!(:cookies) { Recipe.create!(name: 'Sugar Cookies', complexity: 1, genre: 'dessert') }
 
 		let!(:biscuits_ingredients_1) { RecipeIngredient.create!(recipe: biscuits, ingredient: flour)}
 		let!(:biscuits_ingredients_2) { RecipeIngredient.create!(recipe: biscuits, ingredient: eggs)}
@@ -19,12 +19,12 @@ RSpec.describe 'Recipes Show Page', type: :feature do
 		it 'I see recipe name, complexity and genre' do
 			visit "/recipes/#{biscuits.id}"
 			
+			expect(page).to have_content(biscuits.name)
+			expect(page).to have_content("Complexity: #{biscuits.complexity}")
+			expect(page).to have_content("Genre: #{biscuits.genre}")
 			expect(page).to have_content(flour.name)
-			expect(page).to have_content(flour.cost)
 			expect(page).to have_content(eggs.name)
-			expect(page).to have_content(eggs.cost)
 			expect(page).to have_content(butter.name)
-			expect(page).to have_content(butter.cost)
 		end
 	end
 end
