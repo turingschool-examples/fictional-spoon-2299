7fa0d7605fc8e5fae78832a7daeb1f8992c78c84
Harrison Ryan <hwryan12@gmail.com>
2023-02-13 17:42:58 -0600

    feature: Complete user story 1

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,7 +1,7 @@
 class IngredientsController < ApplicationController
   def index
-    @ingredient = Ingredient.find(params[:id])
-    binding.pry
+    # binding.pry
+    @ingredients = Ingredient.all
   end
 
   private
--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -3,14 +3,12 @@ require 'rails_helper'
 RSpec.describe "Ingredient index page" do
 
   describe 'as a visitor' do
-    before :each do
-      @ingredient = Ingredient.create!(name: "Pickles", cost: 3)
-    end
+      let!(:pickles) { Ingredient.create!(name: "Pickles", cost: 3) }
     it "I see a list of all the ingredients including their name and cost" do
       visit '/ingredients'
   
-      expect(page).to have_content(@ingredient.name)
-      expect(page).to have_content(@ingredient.cost)
+      expect(page).to have_content(pickles.name)
+      expect(page).to have_content(pickles.cost)
     end
   end
 end 
