bfd0ad4722e60e1b3f8e02451d5a1091e3f9a85b
James Taylor <jrtaylor0614@gmail.com>
2023-02-12 22:12:32 -0700

    Adds modle test and show test for recipe  #total_cost

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -23,5 +23,29 @@ RSpec.describe 'recipe show page' do
       expect(page).to have_content(ingredient_3.name)
       expect(page).to have_content(ingredient_4.name)
     end 
+
+    it 'shows the total cost of recipe ingredients' do
+      recipe_1 = Recipe.create!(name: 'Strip Steak', complexity: 1, genre: 'American?')
+      ingredient_1 = Ingredient.create!(name: 'salt', cost: 12)
+      ingredient_2 = Ingredient.create!(name: 'pepper', cost: 14)
+      ingredient_3 = Ingredient.create!(name: 'garlic', cost: 13)
+      ingredient_4 = Ingredient.create!(name: 'New York Strip', cost: 25)
+      recipe_ingredient_1 = RecipeIngredient.create!(recipe: recipe_1, ingredient: ingredient_1)
+      recipe_ingredient_2 = RecipeIngredient.create!(recipe: recipe_1, ingredient: ingredient_2)
+      recipe_ingredient_3 = RecipeIngredient.create!(recipe: recipe_1, ingredient: ingredient_3)
+      recipe_ingredient_4 = RecipeIngredient.create!(recipe: recipe_1, ingredient: ingredient_4)
+
+      visit "/recipes/#{recipe_1.id}"
+
+      expect(page).to have_content(recipe_1.name)
+      expect(page).to have_content(recipe_1.complexity)
+      expect(page).to have_content(recipe_1.genre)
+      expect(page).to have_content(ingredient_1.name)
+      expect(page).to have_content(ingredient_2.name)
+      expect(page).to have_content(ingredient_3.name)
+      expect(page).to have_content(ingredient_4.name)
+      expect(page).to have_content(recipe_1.total_cost)
+
+    end
   end 
 end 
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe Recipe, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
   end
+
+  describe '#total_cost' do 
+    it "returns the total cost of ingredients" do 
+      recipe_1 = Recipe.create!(name: 'Strip Steak', complexity: 1, genre: 'American?')
+      ingredient_1 = Ingredient.create!(name: 'salt', cost: 12)
+      ingredient_2 = Ingredient.create!(name: 'pepper', cost: 14)
+      ingredient_3 = Ingredient.create!(name: 'garlic', cost: 13)
+      ingredient_4 = Ingredient.create!(name: 'New York Strip', cost: 25)
+      recipe_ingredient_1 = RecipeIngredient.create!(recipe: recipe_1, ingredient: ingredient_1)
+      recipe_ingredient_2 = RecipeIngredient.create!(recipe: recipe_1, ingredient: ingredient_2)
+      recipe_ingredient_3 = RecipeIngredient.create!(recipe: recipe_1, ingredient: ingredient_3)
+      recipe_ingredient_4 = RecipeIngredient.create!(recipe: recipe_1, ingredient: ingredient_4)
+
+      expect(recipe_1.total_cost).to eq(64)
+    end
+  end
+
 end
