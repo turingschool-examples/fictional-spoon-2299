7bfd6966898089657033413b28783e8017b441cc
Melony Franchini <meltravelz@gmail.com>
2023-02-12 19:48:37 -0700

    Add US2 / feature test for recipe show page

---
+++ spec/features/recipes/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe '/recipes/:id', type: :feature do
+  let!(:pizza) { Recipe.create!(name: "pizza", complexity: 3, genre: "Yummy") }
+
+  let!(:tomato) { Ingredient.create!(name: "tomato", cost: 2) }
+  let!(:mozzarella) { Ingredient.create!(name: "mozzarella", cost: 5) }
+  let!(:garlic) { Ingredient.create!(name: "garlic", cost: 1) }
+
+  let!(:recipe_ingredient1) { RecipeIngredient.create!(recipe: pizza, ingredient: tomato) }
+  let!(:recipe_ingredient2) { RecipeIngredient.create!(recipe: pizza, ingredient: tomato) }
+  let!(:recipe_ingredient3) { RecipeIngredient.create!(recipe: pizza, ingredient: tomato) }
+
+  it 'US2 - should display ' do
+    visit "/recipes/#{pizza.id}"
+
+    expect(page).to have_content("Recipe for #{pizza.name}")
+    expect(page).to have_content("Complexity: #{pizza.complexity}")
+    expect(page).to have_content("Genre: #{pizza.genre}")
+    expect(page).to have_content("Must-Have Ingredients:")
+    expect(page).to have_content("-#{tomato.name}")
+    expect(page).to have_content("-#{mozzarella.name}")
+    expect(page).to have_content("-#{garlic.name}")
+  end
+end
