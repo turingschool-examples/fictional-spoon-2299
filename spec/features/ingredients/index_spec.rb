a69e560a85a39e1eb5b833f41bafba727ba734fe
Melony Franchini <meltravelz@gmail.com>
2023-02-12 19:39:23 -0700

    Add US1 / ing index shows all names & costs

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -5,18 +5,12 @@ RSpec.describe '/ingredients', type: :feature do
   let!(:mozzarella) { Ingredient.create!(name: "mozzarella", cost: 5) }
   let!(:garlic) { Ingredient.create!(name: "garlic", cost: 1) }
 
-  describe 'user story 1' do
-    it 'should display list of all ingredients & attributes' do
-      visit '/ingredients'
-      
-      expect(page).to have_content("Name: #{tomato.name}")
-      expect(page).to have_content("Cost: #{tomato.cost}")
+  it 'US1 - should display list of all ingredients & attributes' do
+    visit '/ingredients'
 
-      expect(page).to have_content("Name: #{mozzarella.name}")
-      expect(page).to have_content("Name: #{mozzarella.cost}")
-
-      expect(page).to have_content("Name: #{garlic.name}")
-      expect(page).to have_content("Name: #{garlic.cost}")
-    end
+    expect(page).to have_content("#{tomato.name}: #{tomato.cost}")
+    expect(page).to have_content("#{mozzarella.name}: #{mozzarella.cost}")
+    expect(page).to have_content("#{garlic.name}: #{garlic.cost}")
+    save_and_open_page
   end
 end
