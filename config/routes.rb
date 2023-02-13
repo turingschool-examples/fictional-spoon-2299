2972efd41e9b7ad1e12e672856fdddf272c2d3f2
Melony Franchini <meltravelz@gmail.com>
2023-02-12 19:50:59 -0700

    Add US2 / routes & controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
   get '/ingredients', to: 'ingredients#index'
+
+  get '/recipes/:id', to: 'recipes#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
