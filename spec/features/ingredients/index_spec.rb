35fca40e36bfca2a2e6aa489a87de1df56d5d58f
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-02-14 15:56:28 -0700

    Add Setup for user story 1 feature test and test database

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Ingredients, type: :feature do
   let!(:cilantro) { Ingredient.create!(name: "Cilantro", cost: 1) }
-  let!(:Onion) { Ingredient.create!(name: "Onion", cost: 2) }
+  let!(:onion) { Ingredient.create!(name: "Onion", cost: 2) }
   let!(:olives) { Ingredient.create!(name: "Olives", cost: 3) }
-  let!(:spaghetti) Recipe.create!(name: "Spaghetti", complexity: 3, genre: "Italian")
-  let!(:pizza) Recipe.create!(name: "Pizza", complexity: 4, genre: "Italian")
-  let!(:garden_salad) Recipe.create!(name: "Garden Salad", complexity: 2, genre: "Greek")
+  let!(:spaghetti) { Recipe.create!(name: "Spaghetti", complexity: 3, genre: "Italian") }
+  let!(:pizza) { Recipe.create!(name: "Pizza", complexity: 4, genre: "Italian") }
+  let!(:garden_salad) { Recipe.create!(name: "Garden Salad", complexity: 2, genre: "Greek") }
 
-  describe 'relationships' do
+  describe 'As a visitor,' do
+    context "when I visit '/ingredients'," do
+      it "I see a list of all the ingredients including their name and cost" do
+        visit '/ingredients'
+
+        expect(page).to have_content("Ingredients:")
+        expect(page).to have_content(cilantro.name)
+        expect(page).to have_content(cilantro.cost)
+        expect(page).to have_content(onions.name)
+        expect(page).to have_content(onions.cost)
+        expect(page).to have_content(olives.name)
+        expect(page).to have_content(olives.cost)
+      end
+    end
   end
 end
