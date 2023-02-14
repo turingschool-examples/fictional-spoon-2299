798de948f6d863ab5e171359dc9469c2e72fd26b
Bobby Luly <bobbyluly@gmail.com>
2023-02-14 12:12:21 -0700

    Test: create test for user story 2

---
+++ spec/features/recipes/show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe '/ingredients', type: :feature do
+  # let!(:spaghetti) { Recipe.create!(name: 'Spaghetti', complexity: 3, genre: 'Italian') }
+  let!(:burger_patty) { Recipe.create!(name: 'Burger Patty', complexity: 2, genre: 'American') }
+  let!(:ground_beef) { Ingredient.create!(name: 'Ground Beef', cost: 8) }
+  let!(:salt) { Ingredient.create!(name: 'Salt', cost: 2) }
+  let!(:pepper) { Ingredient.create!(name: 'Pepper', cost: 3) }
+
+  before do
+    RecipeIngredient.create!(recipe: burger_patty, ingredient: ground_beef)
+    RecipeIngredient.create!(recipe: burger_patty, ingredient: salt)
+    RecipeIngredient.create!(recipe: burger_patty, ingredient: pepper)
+  end
+
+  it 'shows a list of all ingredients including their name and cost' do
+    visit "/recipes/#{burger_patty.id}"
+    
+    # save_and_open_page
+    expect(page).to have_content("Recipe: #{burger_patty.name}")
+    expect(page).to have_content("Complexity: #{burger_patty.complexity}")
+    expect(page).to have_content("Genre: #{burger_patty.genre}")
+    expect(page).to have_content("Ingredients:")
+    expect(page).to have_content("#{ground_beef.name}")
+    expect(page).to have_content("#{salt.name}")
+    expect(page).to have_content("#{pepper.name}")
+  end
+end
