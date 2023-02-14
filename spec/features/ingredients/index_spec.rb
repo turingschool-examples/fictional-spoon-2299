884ad480c15686e37316c9888a97d069ed8a09f9
Chris Crane <chrisjcrane@gmail.com>
2023-02-13 17:12:34 -0700

    create test for user story 1

---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Ingredients Index Page", type: :feature do
+
+  before(:each) do
+    @ingredient_1 = Ingredient.create(name: "Tuna", cost: 5)
+    @ingredient_2 = Ingredient.create(name: "Bell Pepper", cost: 2)
+    @ingredient_3 = Ingredient.create(name: "Poppyseed Dressing", cost: 6)
+  end
+  
+  describe "User Story 1, as a visitor visiting '/ingredients'" do
+    it 'lists all the ingredients with their name and cost' do
+      visit "/ingredients"
+
+      expect(page).to have_content("Tuna")
+      expect(page).to have_content("5")
+      expect(page).to have_content("Bell Pepper")
+      expect(page).to have_content("2")
+      expect(page).to have_content("Poppyseed Dressing")
+      expect(page).to have_content("6")
+    end
+  end
+end
