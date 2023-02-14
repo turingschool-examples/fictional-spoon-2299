edc48d367a9dbff65a32d37ba80e823d6dbfea04
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-02-14 12:29:40 -0700

    Add formatting changes

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -1,16 +1,12 @@
 require 'rails_helper'
 
-# User Story 1
 
-# As a visitor,
-# When I visit '/ingredients'
-# I see a list of all the ingredients including their name and cost
-# (e.g. "Ground Beef: 2"
-#      "Salt: 4")
 
 RSpec.describe '/ingredients' do
   let!(:ingredient1) { Ingredient.create!(name: "Ground Beef", cost: 2) }
-
+  
+  # User Story 1
+  
   describe 'As a visitor' do
     describe 'When I visit /ingredients' do
       it 'I see a list of all the ingredients including their name and cost' do
