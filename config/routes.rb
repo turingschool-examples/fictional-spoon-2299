bfefe2dddb248b46ebab1f16ec4773694cbe515a
David Marino <davidjmarino8@gmail.com>
2023-02-14 14:01:22 -0500

    User Story 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/ingredients', to: 'ingredients#index'
+
+  get '/recipes/:id', to: 'recipes#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ spec/features/recipes/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Ingredient, type: :feature do
+
+  let!(:beef) {Ingredient.create!(name: "Ground Beef", cost: 2)}
+  let!(:carrot) {Ingredient.create!(name: "Carrot", cost: 1)}
+  let!(:ham) {Ingredient.create!(name: "Ham", cost: 3)}
+  let!(:stew) {Recipe.create!(name: "Beef Stew", complexity: 5, genre: "Stew")}
+  let!(:pie) {Recipe.create!(name: "Ham Pie", complexity: 10, genre: "Weird")}
+  
+  it 'shows all the attributes of all ingredients' do
+    RecipeIngredient.create!(recipe: stew, ingredient: beef)
+    RecipeIngredient.create!(recipe: stew, ingredient: carrot)
+
+    visit "/recipes/#{stew.id}"
+
+    expect(page).to have_content("#{stew.name}")
+    expect(page).to have_content("#{stew.complexity}")
+    expect(page).to have_content("#{stew.ingredients.first.name}")
+    expect(page).to have_content("#{stew.ingredients.last.name}")
+  end
+end
