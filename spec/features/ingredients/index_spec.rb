ff531575491b4ad4659dcb74ca1a48dac2e7b0f9
Dawson Timmons <DawsonTimmons@gmail.com>
2023-02-14 13:09:40 -0700

    Complete Extenstion 1 and passing test

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -1,15 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "The ingredients index" do
-  it "shows the visitor a list of all the ingredients including their name and cost" do
-    ingredient_1 = Ingredient.create!(name: 'Beans', cost: 420)
-    ingredient_2 = Ingredient.create!(name: 'Banana', cost: 90)
+  describe "User Story 1" do
+    it "shows the visitor a list of all the ingredients including their name and cost" do
+      ingredient_1 = Ingredient.create!(name: 'Beans', cost: 420)
+      ingredient_2 = Ingredient.create!(name: 'Banana', cost: 90)
 
-    visit '/ingredients'
+      visit '/ingredients'
 
-    expect(page).to have_content(ingredient_1.name)
-    expect(page).to have_content(ingredient_1.cost)
-    expect(page).to have_content(ingredient_2.name)
-    expect(page).to have_content(ingredient_2.cost)
+      expect(page).to have_content(ingredient_1.name)
+      expect(page).to have_content(ingredient_1.cost)
+      expect(page).to have_content(ingredient_2.name)
+      expect(page).to have_content(ingredient_2.cost)
+    end
+  end
+
+  describe "Extension 1" do
+    it "shows the visitor a a list of all the ingredients including their name and cost, sorted alphabetically" do 
+      ingredient_1 = Ingredient.create!(name: 'Beans', cost: 420)
+      ingredient_2 = Ingredient.create!(name: 'Banana', cost: 90)
+      @ingredients = Ingredient.all.order(name: :asc)
+
+      visit '/ingredients'
+
+      expect(@ingredients).to eq([ingredient_2, ingredient_1])
+
+    end
   end
 end
