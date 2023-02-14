5713dff3aa672b3ec248fa4827f3723ba7fcd36a
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-14 08:37:50 -0700

    Test: add test for total cost of recipe

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -14,8 +14,15 @@ require 'rails_helper'
           expect(page).to have_content(recipe_1.complexity)
           expect(page).to have_content(recipe_1.genre)
           expect(page).to have_content("Ingredients")
-          expect(page).to have_content(ingredient_1.name)
-          
+          expect(page).to have_content(ingredient_1.name)   
+      end
+
+      it 'will display the total cost of all of the ingredients of a recipe' do 
+        recipe_1 = Recipe.create!(name: "lasagna", complexity: 2, genre: "italian")
+        ingredient_1 = recipe_1.ingredients.create!(name: "Salt", cost: 1)
+        ingredient_2 = Ingredient.create!(name: "Pepper", cost: 2)
+
+        expect(page).to have_content(recipe_1.cost)
       end
     end
   end
