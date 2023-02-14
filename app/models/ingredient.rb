09fd5d404e78a1dfcbb12f46c3d674031ef350a0
Chris Crane <chrisjcrane@gmail.com>
2023-02-13 18:21:28 -0700

    uncomment out test for recipe data validation and pass

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ApplicationRecord
   validates :name, presence: true
-  validates :cost, presence: true
+  validates :cost, presence: true, numericality: true
   
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
+  validates :name, presence: true
+  validates :complexity, presence: true, numericality: true
+  validates :genre, presence: true
+
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
 
