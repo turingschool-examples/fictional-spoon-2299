cfe96981ccf9922354fb4fa300f6837ead9a5b47
Dawson Timmons <DawsonTimmons@gmail.com>
2023-02-14 12:50:48 -0700

    Complete User Story 3 with passing tests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,5 +2,6 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     @ingredients = @recipe.ingredients
+    @total_cost =  @ingredients.pluck(:cost).sum
   end
 end
--- spec/features/recipies/show_spec.rb
+++ spec/features/recipies/show_spec.rb
@@ -1,24 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe "The recipie show" do
-  it "shows the visitor the recipie with it's attributes" do
-    recipe_1 = Recipe.create!(name: "Bean Soup", complexity: "Hard", genre: "Down Bad")
+  describe "User Story 2" do
+    it "shows the visitor the recipie with it's attributes" do
+      recipe_1 = Recipe.create!(name: "Bean Soup", complexity: "Hard", genre: "Down Bad")
 
-    visit "/recipes/#{recipe_1.id}"
+      visit "/recipes/#{recipe_1.id}"
 
-    expect(page).to have_content(recipe_1.name)
-    expect(page).to have_content(recipe_1.complexity)
-    expect(page).to have_content(recipe_1.genre)
+      expect(page).to have_content(recipe_1.name)
+      expect(page).to have_content(recipe_1.complexity)
+      expect(page).to have_content(recipe_1.genre)
+    end
+
+    it "shows a list of ingredients for the receipe" do
+      recipe_1 = Recipe.create!(name: "Bean Soup", complexity: "Hard", genre: "Down Bad")
+      ingredient_1 = Ingredient.create!(name: "Beans", cost: 420)
+
+      recipe_ingredient_1 = RecipeIngredient.create!(recipe_id: recipe_1.id, ingredient_id: ingredient_1.id)
+      
+      visit "/recipes/#{recipe_1.id}"
+
+      expect(page).to have_content(ingredient_1.name)
+    end
   end
 
-  it "shows a list of ingredients for the receipe" do
-    recipe_1 = Recipe.create!(name: "Bean Soup", complexity: "Hard", genre: "Down Bad")
-    ingredient_1 = Ingredient.create!(name: "Beans", cost: 420)
+  describe "User Story 3" do
+    it "Shows the vistor the total cost of all the ingredients" do
+      recipe_1 = Recipe.create!(name: "Bean Soup", complexity: "Hard", genre: "Down Bad")
+      ingredient_1 = Ingredient.create!(name: "Beans", cost: 420)
+      ingredient_2 = Ingredient.create!(name: "Water", cost: 80)
 
-    recipe_ingredient_1 = RecipeIngredient.create!(recipe_id: recipe_1.id, ingredient_id: ingredient_1.id)
-    
-    visit "/recipes/#{recipe_1.id}"
+      recipe_ingredient_1 = RecipeIngredient.create!(recipe_id: recipe_1.id, ingredient_id: ingredient_1.id)
+      recipe_ingredient_2 = RecipeIngredient.create!(recipe_id: recipe_1.id, ingredient_id: ingredient_2.id)
 
-    expect(page).to have_content(ingredient_1.name)
+      visit "/recipes/#{recipe_1.id}"
+
+      expect(page).to have_content("Total Cost: 500")
+
+    end
   end
 end
