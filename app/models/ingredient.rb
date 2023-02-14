998272bccb0cdf9e41d136c9268f05e1ccc7f05e
Chris Crane <chrisjcrane@gmail.com>
2023-02-13 18:18:24 -0700

    uncomment out ingredient validation test and pass with written validations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,7 @@
 class Ingredient < ApplicationRecord
+  validates :name, presence: true
+  validates :cost, presence: true
+  
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
 end
