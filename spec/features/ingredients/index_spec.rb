e07e66e041f37c0290b64438cf84cb45a383c5f0
Isaac Alter <i_alter@yahoo.com>
2023-02-14 15:35:44 -0700

    Add feat: test logic for User Story 1

---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Ingredients Index' do
+  describe 'When I visit /ingredients' do
+    before(:each) do
+      @ingredient1 = Ingredient.create(name: 'Filet Minon', cost: 5)
+      @ingredient2 = Ingredient.create(name: 'Paprika', cost: 2)
+    end
+    it 'shows a list of all ingredients, including their name and cost: "Ground Beef: 2"' do
+      visit "/ingredients"
+
+      expect(page).to have_content("#{@ingredient1.name}: #{@ingredient1.cost}")
+      expect(page).to have_content("#{@ingredient2.name}: #{@ingredient2.cost}")
+    end
+  end
+end
