60eed0a5231e11402b8097a6604039b3302e8bd1
Brian Hayes <Brian.hayes1200@gmail.com>
2023-02-14 08:50:49 -0700

    Feat: add functionality to display total cost of a recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,13 @@
 class Recipe < ApplicationRecord
  has_many :recipe_ingredients
  has_many :ingredients, through: :recipe_ingredients
+
+ def cost
+  cost = 0
+  ingredients = self.ingredients 
+  ingredients.each do |ingredient|
+    cost += ingredient.cost
+  end
+  cost
+ end
 end
