b8cb308c0beec1c1dd4b63d5de5186d8aec74c07
Harrison Ryan <hwryan12@gmail.com>
2023-02-13 18:32:05 -0600

    feature: Create recipe model method #total_cost and test

--- spec/features/recipes/show_spec.rb
+++ spec/features/recipes/show_spec.rb
@@ -3,9 +3,19 @@ require 'rails_helper'
 RSpec.describe "Recipe show page" do
 
   describe 'as a visitor' do
-      let!(:tacos) { Recipe.create!(name: "Tacos", complexity: 1, genre: "Mexican") }
-      let!(:tortillas) { Ingredient.create!(name: "Tortillas", cost: 2) }
-      let!(:recipe_ingredient) { RecipeIngredient.create!(recipe: tacos, ingredient: tortillas) }
+    let!(:tacos) { Recipe.create!(name: "Breakfast Tacos", complexity: 1, genre: "Mexican") }
+    let!(:tortillas) { Ingredient.create!(name: "Tortillas", cost: 2) }
+    let!(:eggs) { Ingredient.create!(name: "Eggs", cost: 100) }
+    let!(:potato) { Ingredient.create!(name: "Potato", cost: 1) }
+    let!(:cheese) { Ingredient.create!(name: "Cheese", cost: 4) }
+
+    before :each do
+      RecipeIngredient.create!(recipe: tacos, ingredient: tortillas)
+      RecipeIngredient.create!(recipe: tacos, ingredient: eggs)
+      RecipeIngredient.create!(recipe: tacos, ingredient: potato)
+      RecipeIngredient.create!(recipe: tacos, ingredient: cheese)
+    end
+
     it "I see the recipe's name, complexity and genre" do
       visit "/recipes/#{tacos.id}"
   
@@ -19,6 +29,13 @@ RSpec.describe "Recipe show page" do
   
       expect(page).to have_content(tortillas.name)
       expect(page).to have_content(tortillas.cost)
+    end
+
+    it "I see the total cost of all of the ingredients in the recipe" do
+      visit "/recipes/#{tacos.id}"
+  
+      expect(page).to have_content("Total Cost:")
+      expect(page).to have_content("107")
     end
   end
 end 
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe Recipe, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
   end
+
+  describe "#instance methods" do
+  let!(:tacos) { Recipe.create!(name: "Breakfast Tacos", complexity: 1, genre: "Mexican") }
+  let!(:tortillas) { Ingredient.create!(name: "Tortillas", cost: 2) }
+  
+  before :each do
+    RecipeIngredient.create!(recipe: tacos, ingredient: tortillas)
+  end
+
+    describe "#total_cost" do
+      it "returns the total cost of all ingredients in a recipe" do
+        expect(tacos.total_cost).to eq(2)
+      end
+    end
+  end
 end
