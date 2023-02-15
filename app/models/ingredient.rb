def32da65ddcb2c86d08d1df903b9d49d616b000
Isaac Alter <i_alter@yahoo.com>
2023-02-14 17:19:29 -0700

    Add feat: EXTENSION 1 Apologies, did not realize I hadn't switched branches. Otherwise, implemented testing logic to detemine alphabetical order of ingredients in both the index_spec and the ingredient_spec for a model method #self.ordered_alphabetically that has passed its model test

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,8 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
+
+  def self.ordered_alphabetically
+    order(:name)
+  end
 end
