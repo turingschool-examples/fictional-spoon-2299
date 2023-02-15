e9096f0b0e1cac58fbb53707d7e8894058c565f5
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-02-14 17:28:45 -0700

    Add: passing feature test, user story 2 done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,6 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
-    require 'pry'; binding.pry
     @recipe_ingredients = RecipeIngredient.find_ingredients(params[:id])
   end
 end
