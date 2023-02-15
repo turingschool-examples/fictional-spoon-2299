3e7cf284da6e28ff12f7981298978cee2c1bc672
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-02-14 17:26:41 -0700

    Add: model test #self.find_ingredients

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -3,6 +3,8 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :ingredient
   
   def self.find_ingredients(recipe_param)
-    where(recipe_id: recipe_param)
+    found = where(recipe_id: recipe_param).map do |result|
+      result.ingredient
+    end
   end
 end
--- spec/models/recipe_ingredient_spec.rb
+++ spec/models/recipe_ingredient_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe RecipeIngredient, type: :model do
+  before do
+    @pizza = Recipe.create!(name: "Pizza", complexity: 4, genre: "Italian")
+    @onion = @pizza.ingredients.create!(name: "Onion", cost: 2)
+    @olives = @pizza.ingredients.create!(name: "Olives", cost: 3)
+  end
 
   describe 'relationships' do
     it {should belong_to :recipe}
     it {should belong_to :ingredient}
   end
 
+  describe '#find_ingredients' do
+    it 'finds all ingredients associated to a recipe' do
+      expect(RecipeIngredient.find_ingredients(@pizza.id)).to eq([@onion, @olives])
+    end
+  end
 end
